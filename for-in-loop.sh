5d10e00a3f4f95bc032f363f7c4287591bedcd84
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2023-06-14 19:19:59 -0400

    chore: reformat comments

--- for-in-loop.sh
+++ for-in-loop.sh
@@ -5,22 +5,28 @@
 function displayArray()
 {
   i=0
-  iterations=$#  ## buck-hash is the bash equivelent of arguments.lenth
-  length_minus_one='((iterations--))' ##used by itself, decrement syntax requires double parenthesis, used to set a variable it also requires quotes (either kind)
-  echo -n "(";  ## bash arrays are enclosed in parenthesis instead of square brackets
+  ## buck-hash is the bash equivalent of arguments.length
+  iterations=$#
+  ##used by itself, decrement syntax requires double parenthesis, used to set a variable it also requires quotes (either kind)
+  length_minus_one='((iterations--))'
+  ## bash arrays are enclosed in parenthesis instead of square brackets
+  echo -n "(";
   for el
-    do  ## bash requires test expressions to be enclosed in double square brackets with a space on each side- will NOT work without the spaces
-    if [[ $i < $length_minus_one ]]  ## add a comma delimiter to all but the last array member
+    do
+    ## bash requires test expressions to be enclosed in double square brackets with a space on each side- will NOT work without the spaces
+    ## add a comma delimiter to all but the last array member
+    if [[ $i < $length_minus_one ]]
       then
       echo -n " $el, "
       else
       echo -n " $el "
     fi
-    ((i++))  ## standalone increment operator requires double parenthesis without the leading '$'
+    ## standalone increment operator requires double parenthesis without the leading '$'
+    ((i++))
     done
   echo ")"
 }
-
-prime=(2 3 5 7 11) ## arrays in bash use parenthesis and blank-space delimiters
-
-displayArray "${prime[@]}"  ## not sure why the '@' symbol and brackets are required but it doesn't work without them...
+## arrays in bash use parenthesis and blank-space delimiters
+prime=(2 3 5 7 11)
+ ## not sure why the '@' symbol and brackets are required but it doesn't work without them...
+displayArray "${prime[@]}"
