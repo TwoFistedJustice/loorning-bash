ab71d705260ffd79d8a3011f46ecd30b138c5e4d
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2023-06-14 22:00:28 -0400

    refactor: tighten up existing code

--- args-pass-by-reference.sh
+++ args-pass-by-reference.sh
@@ -4,10 +4,18 @@
 # Arguments are normally passed by value. But they can be passed reference.
 
 function addOne() {
-  echo $1  # n => n, $n => 12
+  echo "Pass $3 by $2"
+  echo "\${!1} evaluates to: ${!1}" ## ${!1} means `eval \$$1` or $variable
+  echo "\$1 evaluates to: $1"   # n => n, $n => 12
   let $1+=1;
 }
 
 n=12;
-addOne n; # add $ before the n to see what is echo $1 generates
-echo $n
+addOne n "reference" n; # comment this out and decomment the next line to see what $1 generates
+echo "\$n evaluates to: $n"; echo -e
+
+
+p=100;
+addOne $p "value" p;  # 101 and error "attempted assignment to non-variable "
+echo "\$p evaluates to: $p"
+
