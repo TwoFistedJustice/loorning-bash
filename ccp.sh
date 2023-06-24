072babfabb347e820cd78f25a7d6ed3eec2c165c
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2023-06-24 18:53:39 -0400

    fix: remove extra character. add comments

--- ccp.sh
+++ ccp.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 ##ccp.sh
+## From Mastering Linux by Paul Wang, pp153-155
 ## usage: ccp.sh fromDir toDir [file ...]
 
-(( $# >= 2 )) && [[ -d "$1" && -d "$2" ]] \ || { echo usage: $0 fromDir toDir [ file ... ]; exit 1; } ## (A)
+## Check if the number of args is at least two
+## AND args 1,2 are both directories
+## On fail, display help to user (usage) and exit
+## conditional command switches
+## https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Bash-Conditional-Expressions
+(( $# >= 2 )) && [[ -d "$1" && -d "$2" ]] || { echo usage: $0 fromDir toDir [ file ... ]; exit 1; } ## (A)
 
+## set the vars `from` and `to` to the valuesof the args 1,2
 from=$1; to=$2
+## check AGAIN if args are at least two (could make usage a fn and call it to eliminate one if stmt??)
 if (( $# >= 2 )) ## files supplied
-then filenames=${@:3}  ## (B)
+then filenames=${@:3}  ## (B)  ${@:from-index:to-index} is bash's "array.slice"
 else ## all files in fromDir
-  pushd $from
+  pushd $from                                       ## PUSHD - Save and then change the current directory.
   filenames=( * )
-  popd
+  popd                                              ## POPD - Restore the previous value of the current directory.
 fi
 
-for file in "${filenames[@]}"
+for file in "${filenames[@]}"                       ## (D)
 do
     echo $file;
-    if [[ ! -f "$from/$file" ]]                     ## not a regular file
-      then continue                                 ## skip
+    if [[ ! -f "$from/$file" ]]                     ## if not a regular file
+      then continue                                 ## then go to next file
     fi
     if [[ -f "$to/$file" ]]                         ## $file in folder to $to
-      then if [[ "$from/$file" -nt "$to/$file" ]]    ## (E)
-         then
+      then if [[ "$from/$file" -nt "$to/$file" ]]    ## (E) -n string - true if string.length is non zero
+         then                                        ##-t fd - true if file descriptor fd is open and refers to a terminal
            echo /bin/cp \"$from/$file\" "\$to\"
            /bin/cp $from/$file" "$to"
            fi
